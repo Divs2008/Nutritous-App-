a3e880cf7c7afe77f150d2ce04dfdc4934ca7423
Divya Saha <saha.divs@gmail.com>
2023-07-08 21:13:39 -0700

    Initial Commit

---
+++ Nutritous/NutritousApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutritousApp.swift
+//  Nutritous
+//
+//  Created by Angshuman Saha on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct NutritousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
